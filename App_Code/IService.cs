24903173c30bb0451724979bca9cd1a4c15debdb
fabri <fabri@DESKTOP-8L5TLOD>
2024-05-09 15:40:08 -0500

    Agregar archivos de proyecto.

---
+++ App_Code/IService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+// Definición de la interfaz IService que actúa como un contrato para el servicio SOAP.
+[ServiceContract]
+public interface IService
+{
+    // Operación que devuelve un string basado en un valor entero proporcionado.
+    [OperationContract]
+    string GetData(int value);
+
+    // Operación que maneja un tipo complejo, modifica y devuelve el mismo tipo.
+    [OperationContract]
+    CompositeType GetDataUsingDataContract(CompositeType composite);
+}
+
+// Definición del tipo compuesto CompositeType que se utiliza en una de las operaciones de servicio.
+[DataContract]
+public class CompositeType
+{
+    private bool boolValue = true;
+    private string stringValue = "Hello ";
+
+    // Indica si el valor booleano es verdadero o falso.
+    [DataMember]
+    public bool BoolValue
+    {
+        get { return boolValue; }
+        set { boolValue = value; }
+    }
+
+    // Cadena de texto asociada con la instancia de este tipo.
+    [DataMember]
+    public string StringValue
+    {
+        get { return stringValue; }
+        set { stringValue = value; }
+    }
+}
---
+++ App_Code/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Service : IService
+{
+    public string GetData(int value)
+    {
+        return "You entered: {value}";
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        if (composite == null)
+        {
+            throw new ArgumentNullException("composite");
+        }
+        if (composite.BoolValue)
+        {
+            composite.StringValue += "Suffix";
+        }
+        return composite;
+    }
+}
